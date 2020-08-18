770eef5641cb667dffe4c363c5ad2103580b2a94
Ayaz Ayupov <ayaz@releaseband.com>
2020-08-18 13:26:59 +0300

    fff

--- main.go
+++ main.go
@@ -3,8 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/ayupov-ayaz/plugin/hello"
+	"io/ioutil"
+	"log"
 )
 
 func main() {
 	fmt.Println(hello.SayHello("tommy"))
+	files, err := ioutil.ReadDir("./")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		fmt.Println(f.Name())
+	}
 }
