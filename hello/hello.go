69204a7cdcb91accc6b2ca5282b37b80da50a609
Ayaz Ayupov <ayaz@releaseband.com>
2020-08-18 13:04:28 +0300

    minor changed

---
+++ hello/hello.go
@@ -0,0 +1,26 @@
+package hello
+
+import (
+	"fmt"
+	"log"
+	"plugin"
+)
+
+func HelloPlugin() {
+	p, err := plugin.Open("./run/example.so")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hello, err := p.Lookup("Hello")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f, ok := hello.(func(string string) string)
+	if !ok {
+		log.Fatal("fail")
+	}
+
+	fmt.Println(f("All"))
+}
--- main.go
+++ main.go
@@ -1,26 +1,7 @@
-package plugin
+package main
 
-import (
-	"fmt"
-	"log"
-	"plugin"
-)
+import "github.com/aypov-ayaz/plugin/hello"
 
-func HelloPlugin() {
-	p, err := plugin.Open("./run/example.so")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	hello, err := p.Lookup("Hello")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	f, ok := hello.(func(string string) string)
-	if !ok {
-		log.Fatal("fail")
-	}
-
-	fmt.Println(f("All"))
+func main() {
+	hello.HelloPlugin()
 }
